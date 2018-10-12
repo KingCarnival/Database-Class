3f2a0be28c2faeac18a9ecba3da676ed5122ca07
KingCarnival <pb.itoldyouso@gmail.com>
2018-10-11 22:17:59 -0400

    updates

--- SQL Scripts/Clinic V-1.sql
+++ SQL Scripts/Clinic V-1.sql
@@ -42,3 +42,6 @@ CREATE TABLE [Patient]
     [Insurance] [varchar] (70),
 
 )
+
+INSERT INTO Employee (EmployeeID,Name,Gender,Address,Role,Salary)
+VALUES (00001, "John", Male, "450 Doe Lane", Physician,50000.50);
